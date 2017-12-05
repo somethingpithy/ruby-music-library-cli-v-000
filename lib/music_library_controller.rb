20e0172617ae40800155e793e222f3b7870b9c92
Carlota Bindner <cev.bindner@gmail.com>
2017-12-05 20:55:26 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,6 +1,6 @@
 class MusicLibraryController
 
-  def intialize(path = "./db/mp3s")
+  def initialize(path = "./db/mp3s")
     MusicImporter.new(path).import
   end
 
@@ -16,7 +16,7 @@ class MusicLibraryController
       puts "To list all of the songs of a particular genre, enter 'list genre'."
       puts "To play a song, enter 'play song'."
       puts "To quit, type 'exit'."
-      puts"What would you like to do?"
+      puts "What would you like to do?"
 
       input = gets.strip
 
