6dd0c461661ae0252eb251eaefb8bdf3521d83e4
Carlota Bindner <cev.bindner@gmail.com>
2017-12-06 18:02:19 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -44,9 +44,15 @@ class MusicLibraryController
   end
 
   def list_artists
+    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |a, i|
+      puts "#{i}. #{a.name}"
+    end
   end
 
   def list_genres
+    Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
+      puts "#{i}. #{g.name}"
+    end
   end
 
   def list_songs_by_artist
